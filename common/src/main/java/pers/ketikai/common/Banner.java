23e9f3fcfa9ed3424a79c1f40b31c9d4b30d97b7
ketikai <ketikai@idealstate.team>
2025-05-03 22:31:49 +0800

    feat(common): 增加 Bundled, Log, Banner 相关工具
    [skip ci]
    
    Signed-off-by: ketikai <ketikai@idealstate.team>

---
+++ common/src/main/java/pers/ketikai/common/Banner.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2025 ketikai
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pers.ketikai.common;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public abstract class Banner {
+    private static final String FILE_NAME = "/banner.txt";
+
+    public static List<String> lines() {
+        InputStream inputStream = Banner.class.getResourceAsStream(FILE_NAME);
+        if (inputStream == null) {
+            return Collections.emptyList();
+        }
+        try (BufferedReader it = new BufferedReader(new InputStreamReader(inputStream))) {
+            return it.lines().collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
